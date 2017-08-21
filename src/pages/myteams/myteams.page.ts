cb2f7689db3339e39fd03284c25e27738d4f2d68
agcjunior <antonio.jr@outlook.com>
2017-08-21 11:42:16 -0300

    ionic components 1

--- src/pages/myteams/myteams.page.ts
+++ src/pages/myteams/myteams.page.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { tournamentPage } from'../pages';
+import { NavController, LoadingController } from 'ionic-angular';
+import { tournamentPage, TeamHomePage } from '../pages';
+import { EliteApi } from "../../shared/shared";
 
 @Component({
+  selector:'myTeams',
   templateUrl: 'myteams.page.html',
 })
 export class myTeamsPage {
 
-  constructor(private nav: NavController) {
+  favorites = [
+    {
+      team:{"id": 829, "name": "Baltimore Stars", "coach": "James"},
+      tournamentId: "46ebd526-8839-476a-9ba0-8a9b2c07f3c3",
+      tournamentName:"Summer Showdown"
+    },
+    {
+      team:{"id": 842, "name": "DC Assault", "coach": "Bartlett"},
+      tournamentId: "46ebd526-8839-476a-9ba0-8a9b2c07f3c3",
+      tournamentName:"Summer Showdown"
+    }
+  ];
+
+  constructor(private nav: NavController,
+      private loadingController: LoadingController,
+      private eliteApi:EliteApi) {
   }
 
   goToTournaments() {
     this.nav.push(tournamentPage);
+  }
+
+  favoriteTapped($event, favorite) {
+    let loader = this.loadingController.create({
+      content:"Getting Data...",
+      dismissOnPageChange:true
+    });
+    loader.present();
+    this.eliteApi.getTournamentData(favorite.tournamentId)
+    .subscribe(t=> this.nav.push(TeamHomePage, favorite.team));
   }
 }
--- src/pages/teams/teams.page.ts
+++ src/pages/teams/teams.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
+import * as _ from 'lodash';
 import { TeamHomePage } from '../pages';
 import { EliteApi } from "../../shared/shared";
 
@@ -8,21 +9,39 @@ import { EliteApi } from "../../shared/shared";
 })
 export class teamsPage {
 
+  private allTeams:any;
+  private allTeamsDivision:any;
   teams = [];
+
   constructor(private nav: NavController,
      private navParams: NavParams,
-    private eliteApi:EliteApi) {
+    private eliteApi:EliteApi,
+    private loadingController:LoadingController) {
 
   }
 
   ionViewDidLoad() {
     let selectedTourney =  this.navParams.data;
 
-    this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data=> {
-    this.teams = data.teams;
-  });
+    let loader =this.loadingController.create({
+    content:'Getting Data...'
+    });
 
-}
+    loader.present().then(()=> {
+      this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data=> {
+        this.allTeams = data.teams;
+        this.allTeamsDivision =
+        _.chain(data.teams)
+          .groupBy('division')
+          .toPairs()
+          .map(item => _.zipObject(['divisionName','divisionTeams'], item))
+          .value();
+
+        this.teams = this.allTeamsDivision;
+        loader.dismiss();
+        });
+    });
+  }
 
   itemTapped($event, team) {
     this.nav.push(TeamHomePage, team);
